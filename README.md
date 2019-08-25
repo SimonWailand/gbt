30799fb2851160ea9dd5c778e40f6e814eaa7ac7
Damian Wilson <damian@damianwilson.com>
2019-08-25 19:44:23 -0400

    Update library versions and Eclipse License version

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# gbt
+# Generalized Balanced Ternary
+"Generalized Balanced Ternary (GBT) is a hierarchical addressing system for Euclidean space with a useful algebraic structure."
 
 [![Clojars Project](https://img.shields.io/clojars/v/org.clojars.simonwailand/gbt.svg)](https://clojars.org/org.clojars.simonwailand/gbt)
 
-## License
 
-Copyright © 2018 Damian Wilson
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+
+### References
+
+
+
+
+### License
+
+Copyright © 2019 Damian Wilson
+
+Distributed under the Eclipse Public License version 2.0
--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "A library for manipulating Generalized Balanced Ternary"
   :url "https://github.com/SimonWailand/gbt"
 
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Eclipse Public License - v 2.0"
+            :url "http://www.eclipse.org/legal/epl-v20.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.238"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.520"]
                  [criterium "0.4.4"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]])
